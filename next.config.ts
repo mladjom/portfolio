98f4077f09014017af3686fbeff2333149598b95
mladjom <mladenmilentijevic@gmail.com>
2025-03-19 12:35:42 +0100

    dev

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
-/** @type {import('next').NextConfig} */
-const path = require('path');
+import type { NextConfig } from 'next'
 
-const nextConfig = {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'src/styles')],
-    prependData: `@use "variables.scss"; @use "mixins.scss";`,
-    sourceMap: true
-  }
+const nextConfig: NextConfig = {
+
 }
-
-module.exports = nextConfig
+ 
+export default nextConfig
